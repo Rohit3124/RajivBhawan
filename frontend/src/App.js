5e01e93de4cf11d4830694ac1c1edb77fe8b5375
PiyushChittora <piyushchittora35246@gmail.com>
2022-01-12 16:44:57 +0530

    Welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import "./App.css";
 import Navbar from "./Component/Navbar/Navbar";
 import Footer from "./Component/Footer/Footer";
 import RecentNews from "./Component/RecentNews/RecentNews";
+import Welcome from "./Component/Welcome/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Welcome/>
       <Footer />
       <RecentNews />
     </div>
--- frontend/src/Component/Navbar/Navbar.jsx
+++ frontend/src/Component/Navbar/Navbar.jsx
@@ -7,7 +7,35 @@ import "./Navbar.css";
 //Image Imports
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+    <div className="main">
+      <div className="navigation">
+        <nav id="nav-menu-container">
+          <ul classname="nav-menu">
+            <li id="logo" classname="pull-left">
+              <a href="#intro">
+                <h1>RAJIV BHAWAN</h1>
+              </a>
+            </li>
+            <li>
+              <a href="#about">About Bhawan</a>
+            </li>
+            <li>
+              <a href="#facilities">Facilities</a>
+            </li>
+            <li>
+              <a href="#team">Team</a>
+            </li>
+            <li>
+              <a href="#contactus">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    </>
+  );
 };
 
 export default Navbar;
