c4646df55a7c0b2edaf5f13541fcfaf8397e88d3
Rohit3124 <rohitkumawat4493@gmail.com>
2024-08-29 04:53:23 +0530

    Added photos and emails

--- frontend/src/Component/NoticeBoard/noticeBoard.jsx
+++ frontend/src/Component/NoticeBoard/noticeBoard.jsx
@@ -4,57 +4,38 @@ import { Notices } from "./notices";
 
 const NoticeBoard = () => {
   return (
-    <div className="notice-board" id="noticeBoard">
-      {/* <div className="notice-board-header">Notice Board</div>
-      <div className="notice-board-content">
-        <div className="notice-date-subject">
-          <div className="notice-date-header">Date of Notice</div>
-          <div className="notice-subject-header">Subject</div>
-        </div>
-        {Notices.map((notice, index) => (
-          <div key={index} className="notice-item">
-            <div className="notice-date">{notice.date}</div>
-            <div className="notice-subject">
-              {notice.url ? (
-                <a href={notice.url} target="_blank" rel="noopener noreferrer">
-                  {notice.subject}
-                </a>
-              ) : (
-                notice.subject
-              )}
-            </div>
-          </div>
-        ))}
-      </div> */}
-      <div className="notice-board-header">Notice Board</div>
-      <table className="notice-board-content">
-        <thead>
-          <tr>
-            <th className="notice-date-header">Date of Notice</th>
-            <th className="notice-subject-header">Subject</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Notices.map((notice, index) => (
+    <div id="noticeBoard">
+      <div className="notice-board">
+        <div className="notice-board-header">Notice Board</div>
+        <table className="notice-board-content">
+          <thead>
             <tr>
-              <td className="notice-date">{notice.date}</td>
-              <td className="notice-subject">
-                {notice.url ? (
-                  <a
-                    href={notice.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {notice.subject}
-                  </a>
-                ) : (
-                  notice.subject
-                )}
-              </td>
+              <th className="notice-date-header">Date of Notice</th>
+              <th className="notice-subject-header">Subject</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {Notices.map((notice, index) => (
+              <tr key={index}>
+                <td className="notice-date">{notice.date}</td>
+                <td className="notice-subject">
+                  {notice.url ? (
+                    <a
+                      href={notice.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {notice.subject}
+                    </a>
+                  ) : (
+                    notice.subject
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
